cafb5a568590a80742b3d8893c4c2e21d77aa924
Klaus Borges <klausborgs@gmail.com>
2013-05-14 22:44:27 -0300

    Updated euler1.rb

--- euler1.rb
+++ euler1.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 # Euler 1: Multiples of 3 and 5
 
+# Returns the sum of all multiples of 3 and 5 within range
 def sum_multiples_of_3_and_5(range)
-  s = 0
-  range.map { |n| s += n if n % 3 == 0 || n % 5 == 0 }
-
-  return s
+  range.select { |n| n % 3 == 0 || n % 5 == 0 }.inject(:+)
 end
 
 puts sum_multiples_of_3_and_5((1...1000))
